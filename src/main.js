8ad11bf76b93e2535d669757b3f5b7e5cdf5075a
Student <example@example.com>
2019-11-12 13:15:02 -0800

    Add webpack setup.

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// User Interface Logic
+import './styles.css';
+import { pingPong } from './ping-pong';
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
